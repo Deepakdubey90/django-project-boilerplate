95844539b44667bfc68d6a0808f4633804f38206
vinit Kumar <vinit.kumar@changer.nl>
2014-08-02 21:12:47 +0530

    add post and client modelForms

--- newco/apps/clients/urls.py
+++ newco/apps/clients/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.decorators import login_required
-from .views import (adduser, deleteuser)
+from .views import (deleteuser, add_user)
 
 urlpatterns = patterns('',
-    url(r'^add/$', adduser, name='add_user'),
+    url(r'^add/$', add_user, name='add_user'),
     url(r'^delete/$', deleteuser, name='delete_user'),
 )
--- newco/apps/clients/views.py
+++ newco/apps/clients/views.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from newco.apps.clients.forms import ClientForm
 from newco.apps.clients.models import Client
 # Create your views here.
 
-def adduser(request):
-    return HttpResponse('This is so cool')
 
+
+
+def add_user(request):
+
+    if request.method == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+
+            # commit=False means the form doesn't save at this time.
+            # commit defaults to True which means it normally saves.
+            model_instance = form.save(commit=False)
+            model_instance.save()
+            return redirect('/')
+    else:
+        form = ClientForm()
+
+    return render(request, "clients/templates/client_create.html", {'form': form})
 
 def deleteuser(request):
     return HttpResponse('This is not so cool')

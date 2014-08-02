ef77cd6ec9f76df45e89e8d8e2739c0f06e935f2
vinit Kumar <vinit.kumar@changer.nl>
2014-08-02 21:21:10 +0530

    add profile modelforms as well

--- newco/apps/profiles/urls.py
+++ newco/apps/profiles/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse_lazy as reverse
-from .views import PostFormView, add_post
+from .views import add_profile
 
 urlpatterns = patterns('',
-    url(r'add/$', add_post, name='post_create'),
+    url(r'add/$', add_profile, name='profile_create'),
 )
--- newco/apps/profiles/views.py
+++ newco/apps/profiles/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from newco.apps.profiles.forms import ProfileForm
+
+
+def add_profile(request):
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+
+            # commit=False means the form doesn't save at this time.
+            # commit defaults to True which means it normally saves.
+            model_instance = form.save(commit=False)
+            model_instance.save()
+            return redirect('/')
+    else:
+        form = ProfileForm()
+
+    return render(request, "profiles/templates/profile_create.html", {'form': form})

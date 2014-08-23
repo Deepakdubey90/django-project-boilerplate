45038c8f2dde40e3ff3ac55c937201d2f934c3e4
vinit Kumar <vinit.kumar@changer.nl>
2014-08-24 01:24:09 +0530

    fix silly import error

--- newco/apps/clients/urls.py
+++ newco/apps/clients/urls.py
@@ -6,5 +6,4 @@ from .views import (add_user, ClientListView, NewClient)
 urlpatterns = patterns('',
     url(r'^add/$', NewClient.as_view(), name='add_user'),
     url(r'^list/$', ClientListView.as_view(), name='list_users'),
-    url(r'^delete/$', deleteuser, name='delete_user'),
 )

01f874d262bda7e67ce0b78c95bade8157c68d2d
vinit Kumar <vinit.kumar@changer.nl>
2014-08-24 11:15:34 +0530

    cleanup code to make it lighter and cleaner

--- newco/apps/posts/views.py
+++ newco/apps/posts/views.py
@@ -1,28 +1,32 @@
+"""views for post"""
 from django.core.urlresolvers import reverse_lazy as reverse
-from django.shortcuts import render, redirect
-from django.views.generic import FormView, ListView, CreateView
-# Create your views here.
+from django.views.generic import ListView, CreateView
 from newco.apps.posts.forms import PostForm
 from newco.apps.posts.models import Posts
 
 
 class PostMixin(object):
+    """post mixin"""
     model = Posts
 
     def get_context_data(self, **kwargs):
+        """setup post in context"""
         kwargs.update({'object_name': 'Posts'})
         return kwargs
 
 
 class PostFormMixin(PostMixin):
+    """Mixins for PostForm"""
     form_class = PostForm
     template_name = 'posts/templates/post_create.html'
 
 
 class NewPost(PostFormMixin, CreateView):
+    """New Post View"""
     def get_success_url(self):
         return reverse('dashboard:home')
 
 
 class PostListView(ListView):
+    """Post List View"""
     model = Posts

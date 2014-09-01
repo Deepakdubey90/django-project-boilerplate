eac3303aa7d14549056c9868e667bcc9123672e2
vinit Kumar <vinit.kumar@changer.nl>
2014-09-02 00:13:33 +0530

    trying to fix media issues for images

--- newco/urls.py
+++ newco/urls.py
@@ -21,3 +21,7 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+

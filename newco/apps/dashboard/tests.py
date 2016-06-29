1360f1bace5668f617d6322b12b775a1c0f606b6
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-06-29 23:04:33 +0530

    add tests for dashboard code

--- newco/apps/dashboard/tests.py
+++ newco/apps/dashboard/tests.py
@@ -1,14 +1,19 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-
+from django.contrib.auth.models import User
 # Create your tests here.
 
 class DashboardPageTestCase(TestCase):
     def test_home_page(self):
         resp = self.client.get(reverse('dashboard:home'))
-        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.status_code, 302)
 
 
 class DashboardLoginTestCase(TestCase):
     def test_login(self):
-        pass
+        user = User.objects.create_user(username='vinit', password='linux')
+        resp = self.client.post(reverse('dashboard:login'), data={'username': 'root', 'password': 'linux'})
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.post(reverse('dashboard:login'), data={'username': 'vinit', 'password': 'linux'})
+        self.assertEqual(resp.status_code, 302)
+

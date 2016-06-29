2cae1a69f19d01c44d1e627ebe1554e43d1ebbf5
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-06-29 18:12:30 +0530

    add tests

--- newco/apps/clients/tests.py
+++ newco/apps/clients/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from .models import Client
+from django.core.urlresolvers import reverse
+
 
 
 class ClientTestCase(TestCase):
@@ -9,3 +11,14 @@ class ClientTestCase(TestCase):
     def test_clients_are_created(self):
         client1 = Client.objects.get(name='test user1')
         self.assertEqual(client1.name, 'test user1')
+
+class ClientListTestCase(TestCase):
+    def setUp(self):
+        Client.objects.create(name='test user1', email='test@example.com', address='example address')
+        Client.objects.create(name='test user2', email='test@example.com',
+                address='eadxample address')
+
+    def test_clients_are_created(self):
+        resp = self.client.get(reverse('clients:list_users'))
+        self.assertContains(resp, 'test user1')
+        self.assertContains(resp, 'test user2')
